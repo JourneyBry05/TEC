5e94b794f67c6f979c945cf70291ace56c63d359
JourneyBry <bmongenavarro1@gmail.com>
2024-08-04 23:27:27 -0600

    sorters.h documentation

--- II_Semester2024/AlgorithmsAndDataStructureII/sorter/sorters.h
+++ II_Semester2024/AlgorithmsAndDataStructureII/sorter/sorters.h
@@ -1,10 +1,20 @@
+/*
+Declare sort functions that operate on 'PagedArray' objects
+*/
+/*
+Include guards to make sure the content is included just once
+*/
 #ifndef SORTERS_H
 #define SORTERS_H
 
-#include "PagedArray.h"
+#include "PagedArray.h" //Include 'PagedArray' definitions for sorting functions
 
-void quickSort(PagedArray &arr, int left, int right);
-void insertionSort(PagedArray &arr, size_t size);
-void bubbleSort(PagedArray &arr, size_t size);
+/*
+Void function declarations for each sorting algorithm,
+each function receives a reference to a 'PagedArray' object as "PagedArray &arr"
+*/
+void quickSort(PagedArray &arr, int left, int right); //Initial and last index to sort, respectively
+void insertionSort(PagedArray &arr, size_t size); //size: Size of array to be sorted
+void bubbleSort(PagedArray &arr, size_t size); //size: Size of array to be sorted
 
-#endif // SORTERS_H
+#endif
