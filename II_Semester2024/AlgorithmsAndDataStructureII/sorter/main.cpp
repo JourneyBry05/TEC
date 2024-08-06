d9fd2fbd168e8a72701f667d40b5b5dbb2697190
JourneyBry <bmongenavarro1@gmail.com>
2024-08-05 23:38:22 -0600

    main.cpp documentation

--- II_Semester2024/AlgorithmsAndDataStructureII/sorter/main.cpp
+++ II_Semester2024/AlgorithmsAndDataStructureII/sorter/main.cpp
@@ -1,16 +1,29 @@
+/*
+Sorts a large array of integers stored in a binary file using a specified sorting algorithm. 
+It uses a paginated array to handle large datasets efficiently.
+*/
+
 #include <iostream>
 #include <cstring>
 #include <chrono>
 #include "PagedArray.h"
 #include "sorters.h"
 
+/*Main function to execute the sorter program
+argc: The number of command line arguments passed to the program
+*argv: contain the command line arguments
+*/
 int main(int argc, char *argv[]) {
+    // Check for correct number of arguments
     if (argc != 7) {
-        std::cerr << "Usage: sorter -input <INPUT FILE PATH> -output <OUTPUT FILE PATH> -alg <ALGORITMO>" << std::endl;
+        std::cerr << "Usage: sorter -input <INPUT FILE PATH> -output <OUTPUT FILE PATH> -alg <ALGORITHM>" << std::endl;
         return 1;
     }
 
+    // Variables to store input, output file paths and algorithm name
     std::string inputFilePath, outputFilePath, algorithm;
+
+    // Parse command line arguments
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "-input") == 0) {
             inputFilePath = argv[++i];
@@ -21,10 +34,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Create PagedArray from the input file
     PagedArray arr(inputFilePath);
 
+    // Start timing the sorting process
     auto start = std::chrono::high_resolution_clock::now();
 
+    // Perform sorting based on the specified algorithm
     if (algorithm == "QS") {
         quickSort(arr, 0, arr.size() - 1);
     } else if (algorithm == "IS") {
@@ -36,15 +52,19 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // End timing the sorting process
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
 
+    // Write sorted data to output file
     arr.writeToFile(outputFilePath);
-    arr.writeToTextFile(outputFilePath + ".txt"); // Nueva línea para escribir el archivo de texto
-    arr.printStats();
+    arr.writeToTextFile(outputFilePath + ".txt"); // Write data to text file as well
 
+    // Print statistics and time taken
+    arr.printStats();
     std::cout << "Algorithm used: " << algorithm << std::endl;
     std::cout << "Time taken: " << elapsed.count() << " seconds" << std::endl;
 
     return 0;
 }
+
